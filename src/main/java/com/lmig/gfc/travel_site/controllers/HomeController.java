2c6bcac07b5dceb3b0585a18843e521100673171
Karen Alyea <karen.alyea@libertymutual.com>
2017-11-14 18:09:38 -0500

    very cool travel site

---
+++ src/main/java/com/lmig/gfc/travel_site/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.lmig.gfc.travel_site.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HomeController {
+	
+
+	@RequestMapping("/")
+	public ModelAndView showApp() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("travel");
+		return mv;
+	}
+	
+}
